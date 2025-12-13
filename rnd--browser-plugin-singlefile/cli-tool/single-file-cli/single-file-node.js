848359f76e97b36039e83e1e0b432b09bc51e83f
paulAlexSerban <paul.alex.serban@gmail.com>
2025-12-13 23:37:38 +0200

    feat: cleanup

--- rnd--browser-plugin-singlefile/cli-tool/single-file-cli/single-file-node.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import { run } from "./single-file-launcher.js";
-
-await run();
--- rnd--browser-plugin-singlefile/cli-tool/single-file-cli/single-file.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-deno run --allow-read --allow-write --allow-net --allow-env --allow-run "%~dp0\single-file" %*
